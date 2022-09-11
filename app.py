aa951c63d73a4bfe57b323f093cbbb09373cf36a
Vasily <wasjaus@gmail.com>
2022-09-11 19:35:21 +0700

    files added

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastai.vision.all import *
+import gradio as gr
+
+learn_inf = load_learner('export.pkl')
+
+categories = ('Snake', 'Gecko')
+
+def classify_img(img):
+    pred, idx, probs = learn_inf.predict(img)
+    return dict(zip(categories, map(float, probs)))
+
+image = gr.inputs.Image(shape=(192, 192))
+label = gr.outputs.Label()
+examples = ['../input/gecko-snake-examples/gecko.jpg.jpg', '../input/gecko-snake-examples/snake.jpeg']
+
+intf = gr.Interface(fn=classify_img, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
+
